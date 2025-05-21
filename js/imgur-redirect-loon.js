3dcee215536a087336d2fdb620da1a7b745f0b76
S7venYoung <S7venYoung@users.noreply.github.com>
2025-05-21 21:15:09 +0800

    Update imgur-redirect-loon.js

--- js/imgur-redirect-loon.js
+++ js/imgur-redirect-loon.js
@@ -1,13 +1,21 @@
-// imgur-mirror.js
-// 将 imgur 图片跳转到镜像站
-// @author Seven
+// imgur-redirect-loon.js
+// 自动将 imgur 图片请求跳转到镜像站
+// Author: @S7venYoung + ChatGPT
 
-const url = $request.url;
 const mirrorBase = "https://img.noobzone.ru/getimg.php?url=";
+const url = $request.url;
 
-if (url.includes("i.imgur.com")) {
+if (url.match(/^https?:\/\/(i\.)?imgur\.com\/.+\.(jpg|jpeg|png|gif|webp)$/i)) {
   const redirectUrl = mirrorBase + encodeURIComponent(url);
-  $done({ response: { status: 302, headers: { Location: redirectUrl } } });
+  $done({
+    response: {
+      status: 302,
+      headers: {
+        Location: redirectUrl,
+        "Cache-Control": "no-cache"
+      }
+    }
+  });
 } else {
   $done({});
 }
